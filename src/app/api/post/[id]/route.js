51d95e4a9bdba14ab90f1a377b06f8e68081d7e9
YUVAN-cse <rr4030201@email.com>
2026-03-19 13:44:20 +0530

    added a few functions

--- src/app/api/post/[id]/route.js
+++ src/app/api/post/[id]/route.js
@@ -3,3 +3,7 @@ export async function GET( request, {params}) {
     return NextResponse.json({id : id});
 }
 
+export async function POST( request, {params}) {
+    let {id} = await params;
+    return NextResponse.json({id : id});
+}
